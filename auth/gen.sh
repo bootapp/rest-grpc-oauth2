a05619200c41995695472d2a1549173324fff5e4
wangh09 <wanghaoforever@126.com>
2019-11-10 11:19:37 +0800

    add core_common proto

--- auth/gen.sh
+++ auth/gen.sh
@@ -2,4 +2,5 @@
 mkdir -p pb
 protoc -I$GOPATH/src/github.com/bootapp/proto-core \
        --go_out=plugins=grpc:./pb \
+        $GOPATH/src/github.com/bootapp/proto-core/core_common.proto \
         $GOPATH/src/github.com/bootapp/proto-core/dal_auth.proto
