2ce45d2dd8a67d93dfbe2b5c1bd6e3b477d0939a
wangh09 <wanghaoforever@126.com>
2019-11-10 13:33:42 +0800

    remove core_common

--- auth/gen.sh
+++ auth/gen.sh
@@ -2,5 +2,4 @@
 mkdir -p pb
 protoc -I$GOPATH/src/github.com/bootapp/proto-core \
        --go_out=plugins=grpc:./pb \
-        $GOPATH/src/github.com/bootapp/proto-core/core_common.proto \
         $GOPATH/src/github.com/bootapp/proto-core/dal_auth.proto
